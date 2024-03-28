4ba1d56a8d9f50ae329a61bab52a39924eff7cb1
Truc Thanh <trucnguyenthanh56@gmail.com>
2024-03-28 07:56:29 +0700

    init project

---
+++ eCourses/courses/admin.py
@@ -0,0 +1,31 @@
+from django.contrib import admin
+from courses.models import Category, Course
+from django.utils.html import mark_safe
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+from django import forms
+
+class CourseForm(forms.ModelForm):
+    description = forms.CharField(widget=CKEditorUploadingWidget)
+
+    class Meta:
+        model = Course
+        fields = '__all__'
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'created_date', 'active']
+    search_fields = ['name', 'description']
+    list_filter = ['id', 'name', 'created_date']
+    readonly_fields = ['my_image']
+
+    def my_image(self, course):
+        if course.image:
+            return mark_safe(f"<img width='200' src='/static/{course.image.name}' />")
+
+    class Media:
+        css = {
+            'all':[]
+        }
+        js = []
+
+admin.site.register(Category)
+admin.site.register(Course, CourseAdmin)
